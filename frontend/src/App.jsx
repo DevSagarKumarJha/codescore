127f0d88b117d52f5b31417f98005b4719a738d1
devsagarkumarjha <devsagarkumarjha@gmail.com>
2025-05-16 19:29:45 +0530

    Basic setup for authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage, LoginPage, SignUpPage } from "./components";
 
-function App() {
+const App = () => {
+  let authUser = "sagar";
+
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className="flex flex-col-items-center justify-start">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signin"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
